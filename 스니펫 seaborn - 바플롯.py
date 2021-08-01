7229960c5c136a700aa199cb569856e26a9e52b6
석진일 <>
2021-08-02 01:18:06 +0900

    Doc : Create - 바플롯

---
+++ 스니펫 seaborn - 바플롯.py
@@ -0,0 +1,22 @@
+# 시각화 하기
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.rc('font', family='NanumBarunGothic')
+
+plt.figure(figsize=(12, 8)) # 그래프 크기 설정
+
+# 활용 1 - 정렬해서 그리기
+groupby_mean = groupby_mean.sort_values(
+    ['조회'], ascending=False).reset_index(drop=True)  # 이전 인덱스 삭제
+groupby_sum = groupby_sum.sort_values(
+    ['조회'], ascending=False).reset_index(drop=True)
+
+# 조회수 평균 그래프 그리기
+sns.barplot(y='학년반', x='조회', data=groupby_mean)  # 그래프 그리기
+plt.show()
+
+# 활용2 - 조회수 전체 합 그래프 그리기
+plt.figure(figsize=(12, 8))
+sns.barplot(y='학년반', x='조회', data=groupby_sum)
+plt.show()
