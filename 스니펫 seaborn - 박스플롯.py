2ceab29419877a65a44f734258831a8909fbbd76
951237 <951237@gmail.com>
2021-08-02 01:20:41 +0900

    Doc : Update

--- 스니펫 seaborn - 박스플롯.py
+++ 스니펫 seaborn - 박스플롯.py
@@ -4,3 +4,4 @@ plt.figure(figsize=(12, 6))  # 그래프 크기 설정하기
 # x축을 데이터의 크기만큼 설정해서 데이터프레임의 칼럼값으로 그래프 그리기
 sns.lineplot(x=range(585), y=test['src'], data=test)    # x축을 range를 이용해서 피처 갯수만큼 585개를 생성
 sns.lineplot(x=range(585), y=test['predict'], data=test)
+
